3604c1811bbbafabe9fd85b97aeeccb4e31463a0
Jake Miller <jake0miller@gmail.com>
2019-10-20 17:01:19 -0600

    Create factory for making centers, write test for getting all centers

--- spec/requests/api/v1/centers/index_spec.rb
+++ spec/requests/api/v1/centers/index_spec.rb
@@ -2,37 +2,24 @@ require 'rails_helper'
 
 describe 'GET /api/v1/centers' do
   before :each do
-    request_body = { "address": "1331,17th,St,Denver,CO,80202",
-      "phone": "5555555555",
-      "website": "www.relief-center.com",
-      "email": "get-relief@this-center.com", }
+    FactoryBot.create_list(:center, 5)
+    @centers = Center.all
+    expect(@centers.length).to eq(5)
   end
 
-  it 'I can create a new center' do
+  it 'I can get all new centers' do
+    get '/api/v1/centers'
 
-    post '/api/v1/centers', params: request_body.to_json, headers: @headers
+    centers = JSON.parse(response.body, symbolize_names: true)
 
-    centers = Center.all
+    expect(response.status).to eq(200)
+    expect(centers.length).to eq(5)
 
-    expect(centers.length).to eq(1)
-
-    center = centers.last
-
-    expect(center.address).to eq(request_body[:address])
-    expect(center.phone).to eq(request_body[:phone])
-    expect(center.email).to eq(request_body[:email])
-    expect(center.website).to eq(request_body[:website])
-    expect(center.lat).to eq("39.7508132")
-    expect(center.lng).to eq("-104.9967997")
-
-    center = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response.status).to eq(201)
-    expect(center[:address]).to eq(request_body[:address])
-    expect(center[:phone]).to eq(request_body[:phone])
-    expect(center[:email]).to eq(request_body[:email])
-    expect(center[:website]).to eq(request_body[:website])
-    expect(center[:lat]).to eq("39.7508132")
-    expect(center[:lng]).to eq("-104.9967997")
+    # expect(center[:address]).to eq(request_body[:address])
+    # expect(center[:phone]).to eq(request_body[:phone])
+    # expect(center[:email]).to eq(request_body[:email])
+    # expect(center[:website]).to eq(request_body[:website])
+    # expect(center[:lat]).to eq("39.7508132")
+    # expect(center[:lng]).to eq("-104.9967997")
   end
 end
