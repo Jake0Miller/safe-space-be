6891dd288f04d083edb8abb82c05a877e824be3d
Jake Miller <jake0miller@gmail.com>
2019-10-21 14:41:00 -0600

    Set up GraphQL and GraphiQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require "graphiql/rails"
+
 Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
