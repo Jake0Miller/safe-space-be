e231b169d0e94e28f71f56257a2b6a31b86248ed
Jake Miller <jake0miller@gmail.com>
2019-10-22 16:57:30 -0600

    Update db params and add a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require "graphiql/rails"
 
 Rails.application.routes.draw do
+  root 'api/v1/centers#index'
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
