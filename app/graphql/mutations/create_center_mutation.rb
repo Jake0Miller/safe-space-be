46dac7d4c8c354b2918ef5cb85d7f52111e198db
Jake Miller <jake0miller@gmail.com>
2019-10-23 14:19:52 -0600

    Write mutation for creating a center

---
+++ app/graphql/mutations/create_center_mutation.rb
@@ -0,0 +1,21 @@
+module Mutations
+  class CreateCenterMutation < Mutations::BaseMutation
+    argument :address, String, required: true
+    argument :phone, String, required: true
+    argument :website, String, required: true
+    argument :email, String, required: true
+
+    field :center, Types::CenterType, null: true
+    field :errors, [String], null: false
+
+    def resolve(**attributes)
+      coords = Geocoder.new(attributes[:address]).coords
+      center = Center.new(attributes.merge(lat: coords[:lat], lng: coords[:lng]))
+      if center.save
+        { center: center }
+      else
+        { errors: center.errors.full_messages }
+      end
+    end
+  end
+end
