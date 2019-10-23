f417e5e0d107d7bb3852cccff77736f44d99134d
Jake Miller <jake0miller@gmail.com>
2019-10-23 14:19:23 -0600

    Write test for creating a center through graphql

---
+++ spec/requests/centers/index_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe 'GET /api/v1/centers' do
+  before :each do
+    FactoryBot.create_list(:item, 5)
+    @items = Item.all
+
+    @center_1 = FactoryBot.create(:center)
+    @center_2 = FactoryBot.create(:center)
+    @centers = [@center_1, @center_2]
+    @items.each do |item|
+      CenterItem.create!(item: item, center: @center_1)
+    end
+    CenterItem.create!(item: @items.first, center: @center_2)
+    CenterItem.create!(item: @items.last, center: @center_2)
+
+    expect(@items.length).to eq(5)
+    expect(Center.count).to eq(2)
+    expect(CenterItem.count).to eq(7)
+  end
+
+  it 'I can get all centers and their items' do
+    query = { "query" => "{
+              centers {
+                id
+                address
+                phone
+                email
+                website
+                lat
+                lng
+                items {
+                  id
+                  name
+                }
+              }
+            }" }
+    post '/graphql', params: query
+
+    centers = JSON.parse(response.body, symbolize_names: true)[:data][:centers]
+
+    expect(response.status).to eq(200)
+    expect(centers.length).to eq(2)
+    centers.each_with_index do |center,i|
+      expect(center[:id]).to eq(@centers[i].id.to_s)
+      expect(center[:address]).to eq(@centers[i].address)
+      expect(center[:items].first[:id]).to eq(@centers[i].items.first.id.to_s)
+    end
+  end
+end
