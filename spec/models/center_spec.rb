6f77687354e0e21c3d16309d2881c10a849a0a05
Jake Miller <jake0miller@gmail.com>
2019-10-23 15:23:20 -0600

    Add test for center name validation

--- spec/models/center_spec.rb
+++ spec/models/center_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Center, type: :model do
   describe 'Validations' do
+    it {should validate_presence_of :name}
     it {should validate_presence_of :address}
     it {should validate_presence_of :lat}
     it {should validate_presence_of :lng}
