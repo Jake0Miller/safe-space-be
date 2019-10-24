cf89ab8cfe1f0db15490507e637417d6ea936adb
Jake Miller <jake0miller@gmail.com>
2019-10-23 23:17:28 -0600

    Run user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_233722) do
+ActiveRecord::Schema.define(version: 2019_10_24_051359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,17 @@ ActiveRecord::Schema.define(version: 2019_10_20_233722) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.integer "age"
+    t.bigint "center_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["center_id"], name: "index_users_on_center_id"
+  end
+
   add_foreign_key "center_items", "centers"
   add_foreign_key "center_items", "items"
+  add_foreign_key "users", "centers"
 end
