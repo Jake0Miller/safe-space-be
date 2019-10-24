fd5abdb6487a5c6cc9e335d77c9801a59a84825c
Jake Miller <jake0miller@gmail.com>
2019-10-24 16:42:06 -0600

    Write GraphQL query for returning items at a specific center, along with quantity at that center

--- spec/requests/items/index_spec.rb
+++ spec/requests/items/index_spec.rb
@@ -9,9 +9,9 @@ describe 'GET items' do
     @center_2 = FactoryBot.create(:center)
     @centers = [@center_1, @center_2]
     @items.each do |item|
-      CenterItem.create!(item: item, center: @center_1)
+      CenterItem.create!(item: item, center: @center_1, quantity: 7)
     end
-    CenterItem.create!(item: @items.first, center: @center_2)
+    CenterItem.create!(item: @items.first, center: @center_2, quantity: 5)
     CenterItem.create!(item: @items.last, center: @center_2)
 
     expect(@items.length).to eq(5)
@@ -27,6 +27,7 @@ describe 'GET items' do
                 centers {
                   id
                   addressPrint
+                  quantity
                 }
               }
             }" }
@@ -40,7 +41,10 @@ describe 'GET items' do
       expect(item[:id]).to eq(@items[i].id.to_s)
       expect(item[:name]).to eq(@items[i].name)
       expect(item[:centers].first[:id]).to eq(@items[i].centers.first.id.to_s)
+      expect(item[:centers].first[:quantity]).to eq(7)
     end
+    expect(items.first[:centers].last[:quantity]).to eq(5)
+    expect(items.last[:centers].last[:quantity]).to eq(0)
   end
 
   it 'I can get all items for a specific center' do
@@ -48,17 +52,37 @@ describe 'GET items' do
               itemsAtCenter(centerId: #{@center_1.id}) {
                 id
                 name
+                quantity
               }
             }" }
 
     post '/graphql', params: query
 
-    items = JSON.parse(response.body, symbolize_names: true)[:data][:items]
+    items = JSON.parse(response.body, symbolize_names: true)[:data][:itemsAtCenter]
 
-    expect(items.length).to eq(3)
+    expect(items.length).to eq(5)
     items.each_with_index do |item,i|
       expect(item[:id]).to eq(@items[i].id.to_s)
       expect(item[:name]).to eq(@items[i].name)
+      expect(item[:quantity]).to eq(7)
     end
+
+    query = { "query" => "{
+              itemsAtCenter(centerId: #{@center_2.id}) {
+                id
+                name
+                quantity
+              }
+            }" }
+
+    post '/graphql', params: query
+
+    items = JSON.parse(response.body, symbolize_names: true)[:data][:itemsAtCenter]
+
+    expect(items.length).to eq(2)
+    expect(items.first[:id]).to eq(@items.first.id.to_s)
+    expect(items.first[:name]).to eq(@items.first.name)
+    expect(items.first[:quantity]).to eq(5)
+    expect(items.last[:quantity]).to eq(0)
   end
 end
