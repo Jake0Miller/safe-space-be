a74457f469facbd60e030bafa3f0ced10e0dec7c
Jake Miller <jake0miller@gmail.com>
2019-10-24 22:17:43 -0600

    Add create_item to mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_center, mutation: Mutations::CreateCenterMutation
     field :create_user, mutation: Mutations::CreateUserMutation
+    field :create_item, mutation: Mutations::CreateItemMutation
   end
 end
