7c4219bb9fd3273997424a7b8b9ea4fa61e5535e
Jake Miller <jake0miller@gmail.com>
2019-10-26 12:05:13 -0600

    Restyle module line out of mutations

--- app/graphql/mutations/create_item_mutation.rb
+++ app/graphql/mutations/create_item_mutation.rb
@@ -1,20 +1,18 @@
-module Mutations
-  class CreateItemMutation < Mutations::BaseMutation
-    argument :name, String, required: true
-    argument :quantity, Integer, required: true
-    argument :center_id, Integer, required: true
+class Mutations::CreateItemMutation < Mutations::BaseMutation
+  argument :name, String, required: true
+  argument :quantity, Integer, required: true
+  argument :center_id, Integer, required: true
 
-    field :item, Types::ItemType, null: true
-    field :errors, [String], null: false
+  field :item, Types::ItemType, null: true
+  field :errors, [String], null: false
 
-    def resolve(**attributes)
-      item = Item.new(name: attributes[:name])
-      if item.save
-        CenterItem.create(item: item, quantity: attributes[:quantity], center_id: attributes[:center_id])
-        { item: item }
-      else
-        { errors: item.errors.full_messages }
-      end
+  def resolve(**attributes)
+    item = Item.new(name: attributes[:name])
+    if item.save
+      CenterItem.create(item: item, quantity: attributes[:quantity], center_id: attributes[:center_id])
+      { item: item }
+    else
+      { errors: item.errors.full_messages }
     end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,47 +1,45 @@
-module Types
-  class QueryType < Types::BaseObject
-    field :items, [Types::ItemType], null: false
+class Types::QueryType < Types::BaseObject
+  field :items, [Types::ItemType], null: false
 
-    def items
-      Item.all
-    end
+  def items
+    Item.all
+  end
 
-    field :items_at_center, [Types::ItemType], null: false do
-      argument :center_id, Integer, required: true
-    end
+  field :items_at_center, [Types::ItemType], null: false do
+    argument :center_id, Integer, required: true
+  end
 
-    def items_at_center(center_id:)
-      Item.joins(:centers)
-        .select('items.*, center_items.quantity')
-        .where(centers: {id: center_id})
-    end
+  def items_at_center(center_id:)
+    Item.joins(:centers)
+      .select('items.*, center_items.quantity')
+      .where(centers: {id: center_id})
+  end
 
-    field :centers, [Types::CenterType], null: false
+  field :centers, [Types::CenterType], null: false
 
-    def centers
-      Center.all
-    end
+  def centers
+    Center.all
+  end
 
-    field :users, [Types::UserType], null: false
+  field :users, [Types::UserType], null: false
 
-    def users
-      User.all
-    end
+  def users
+    User.all
+  end
 
-    field :user, Types::UserType, null: false do
-      argument :id, ID, required: true
-    end
+  field :user, Types::UserType, null: false do
+    argument :id, ID, required: true
+  end
 
-    def user(id:)
-      User.find(id)
-    end
+  def user(id:)
+    User.find(id)
+  end
 
-    field :users_at_center, [Types::UserType], null: false do
-      argument :center_id, Integer, required: true
-    end
+  field :users_at_center, [Types::UserType], null: false do
+    argument :center_id, Integer, required: true
+  end
 
-    def users_at_center(center_id:)
-      User.where(center_id: center_id)
-    end
+  def users_at_center(center_id:)
+    User.where(center_id: center_id)
   end
 end
