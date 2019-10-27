dc36d720a37b10147d24f64d730666c074a5ced4
Jake Miller <jake0miller@gmail.com>
2019-10-26 22:26:27 -0600

    Create item edit mutation

---
+++ app/graphql/mutations/edit_item_mutation.rb
@@ -0,0 +1,18 @@
+class Mutations::EditItemMutation < Mutations::BaseMutation
+  argument :id, ID, required: true
+  argument :center_id, ID, required: true
+  argument :quantity, Integer, required: true
+
+  field :item, Types::ItemType, null: true
+  field :errors, [String], null: false
+
+  def resolve(**attributes)
+    item = CenterItem.find_by(item_id: attributes[:id], center_id: attributes[:center_id])
+    if item
+      item.update(quantity: attributes[:quantity])
+      { item: item }
+    else
+      { errors: item.errors.full_messages }
+    end
+  end
+end
