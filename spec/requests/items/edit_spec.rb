b5115c7e913a7dc0aa2981892d1a21dda983c73f
Jake Miller <jake0miller@gmail.com>
2019-10-26 22:26:20 -0600

    Write test for editing an items quantity

---
+++ spec/requests/items/edit_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'DELETE user' do
+  before :each do
+    @center_1 = FactoryBot.create(:center)
+    @item_1 = Item.create(name: 'food')
+    @center_item_1 = CenterItem.create(center: @center_1, item: @item_1)
+
+    expect(Item.count).to eq(1)
+    expect(@center_item_1.quantity).to eq(0)
+    expect(CenterItem.count).to eq(1)
+  end
+
+  it 'I can edit an item' do
+    mutation = %(mutation {
+                  editItem(id: #{@item_1.id}, centerId: #{@center_1.id}, quantity: 10) {
+                    item {
+                      quantity
+                    }
+                  }
+                })
+
+    post '/graphql', params: { "query" => mutation }
+
+    expect(@center_item_1.reload.quantity).to eq(10)
+  end
+end
