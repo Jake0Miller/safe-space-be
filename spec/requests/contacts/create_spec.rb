9520169a6d3445214fd4e2c5de49b60b6a52bd6f
Jake Miller <jake0miller@gmail.com>
2019-10-27 15:28:15 -0600

    Write test for creating new contacts

---
+++ spec/requests/contacts/create_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'POST contact' do
+  before :each do
+    @center_1 = FactoryBot.create(:center)
+    @user_1 = @center_1.users.create!(name: 'Jake')
+
+    expect(Contact.count).to eq(0)
+  end
+
+  it 'I can create a new contact' do
+    mutation = %(mutation {
+                  createContact (
+                      name: "Ian"
+                      phone: "555-555-5555"
+                      userId: #{@user_1.id}
+                    ) {
+                    contact {
+                      id
+                      name
+                      phone
+                      userId
+                    }
+                  }
+                })
+
+    post '/graphql', params: { "query" => mutation }
+
+    contacts = Contact.all
+    data_contact = contacts.first
+
+    expect(contacts.length).to eq(1)
+
+    contact = JSON.parse(response.body, symbolize_names: true)[:data][:createContact][:contact]
+    
+    expect(contact[:id]).to eq(data_contact.id.to_s)
+    expect(contact[:name]).to eq(data_contact.name)
+    expect(contact[:phone]).to eq(data_contact.phone)
+  end
+end
