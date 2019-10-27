ba24253180d09b24485efec1099b289c00b444ba
Jake Miller <jake0miller@gmail.com>
2019-10-27 00:19:36 -0600

    Add stub to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,10 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  def stub_json(url, filename)
+    json_response = File.open(filename)
+    stub_request(:get, url).to_return(status: 200, body: json_response)
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
