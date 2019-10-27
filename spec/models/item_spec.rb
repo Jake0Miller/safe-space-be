effcf12ad0a60c61fd032593056e54eb69e30a82
Jake Miller <jake0miller@gmail.com>
2019-10-27 14:51:39 -0600

    Add consumable validation to item model test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'Validations' do
     it {should validate_presence_of :name}
+    it {should validate_presence_of :consumable}
   end
 
   describe 'Relationships' do
