52e629b7b9dcf1e7bf0cdc7a0d5c09dade3a910a
Jake Miller <jake0miller@gmail.com>
2019-10-28 01:33:07 -0600

    Add additional fields to user edit mutation

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,6 +3,9 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :phone, String, null: false
+    field :age, Integer, null: false
+    field :allergies, Boolean, null: false
+    field :diet_restrictions, Boolean, null: false
     field :center, CenterType, null: false
 
     def center
