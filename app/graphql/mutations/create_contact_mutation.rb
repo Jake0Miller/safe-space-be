92b88a5bfe9fbbccea9f18f70b83d144adfdfdce
Jake Miller <jake0miller@gmail.com>
2019-10-28 19:28:06 -0600

    Add twilio SMS to emergency contact creation

--- app/graphql/mutations/create_contact_mutation.rb
+++ app/graphql/mutations/create_contact_mutation.rb
@@ -1,7 +1,10 @@
+require 'twilio-ruby'
+
 class Mutations::CreateContactMutation < Mutations::BaseMutation
   argument :name, String, required: true
   argument :phone, String, required: true
   argument :user_id, Integer, required: true
+  argument :send, Boolean, required: true
 
   field :contact, Types::ContactType, null: true
   field :errors, [String], null: false
@@ -9,6 +12,14 @@ class Mutations::CreateContactMutation < Mutations::BaseMutation
   def resolve(**attributes)
     contact = Contact.new(attributes)
     if contact.save
+      if attributes[:send]
+        client = Twilio::REST::Client.new ENV['T_SID'], ENV['T_KEY']
+        message = client.messages.create(
+            body: "Hello from Ruby",
+            to: "+1#{attributes[:phone]}",
+            from: "+13345680005")
+        render json: 'Message sent successfully'
+      end
       { contact: contact }
     else
       { errors: contact.errors.full_messages }
