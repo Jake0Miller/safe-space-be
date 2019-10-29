dcf1f6d2a9768a24436ff83fffb3872fcf199451
Jake Miller <jake0miller@gmail.com>
2019-10-29 17:37:28 -0600

    Fix item validations to allow false boolean values

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
-  validates_presence_of :name, :consumable
+  validates_presence_of :name
+  validates_inclusion_of :consumable, :in => [true, false]
   has_many :center_items
   has_many :centers, through: :center_items
 end
