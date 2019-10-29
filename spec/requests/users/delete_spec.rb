ed33a98a7d410b75327f8e72002c127f6c530b32
Jake Miller <jake0miller@gmail.com>
2019-10-29 17:46:29 -0600

    Add dependent delete all to users, so their contacts and needs are also deleted

--- spec/requests/users/delete_spec.rb
+++ spec/requests/users/delete_spec.rb
@@ -4,7 +4,8 @@ describe 'DELETE user' do
   before :each do
     @center_1 = FactoryBot.create(:center)
     @user_1 = User.create(name: 'Jake', center: @center_1)
-
+    @user_1.needs.create(name: 'food')
+    @user_1.contacts.create(name: 'Jim', phone: '5')
     expect(User.count).to eq(1)
   end
 
@@ -19,7 +20,9 @@ describe 'DELETE user' do
                 })
 
     post '/graphql', params: { "query" => mutation }
-    
+
     expect(User.count).to eq(0)
+    expect(Need.count).to eq(0)
+    expect(Contact.count).to eq(0)
   end
 end
